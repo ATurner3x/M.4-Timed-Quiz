d6a9c510302dce716fb23cb0d22a0be0f6cfaf6b
Amadeus <a.turner3x@gmail.com>
2023-03-06 22:43:17 -0500

    added game over + submission

--- develop/js/script.js
+++ develop/js/script.js
@@ -35,17 +35,17 @@ let currentQuestionIndex;
 let timeleft;
 let score;
 
-var startButton = document.querySelector('#start-button')
-startButton.addEventListener("click", startQuiz)
+var startButton = document.querySelector('#start-button');
+startButton.addEventListener("click", startQuiz);
 const choicesContainer = document.getElementById("choices");
-const questionText = document.getElementById("question-text")
+const questionText = document.getElementById("question-text");
 
 // Start Quiz
 
 function startQuiz () {
-    var main = document.getElementById("main").style.visibility = "Visible"
-    var titlePage = document.getElementById("quiz-intro").style.visibility = "hidden"
-    var questionsScreen = document.getElementById("question-screen").style.visibility = "visible"
+    var main = document.getElementById("main").style.visibility = "Visible";
+    var titlePage = document.getElementById("quiz-intro").style.visibility = "hidden";
+    var questionsScreen = document.getElementById("question-screen").style.visibility = "visible";
     currentQuestionIndex = 0;
     timeLeft = quizSettings.duration;
     score = 0;
@@ -89,7 +89,7 @@ function displayQuestion() {
 function checkAnswer(answer) {
     if (answer === quizSettings.questions[currentQuestionIndex].answer) {
        // Answer is correct
-    score += 10;
+    score += 1;
     
   } else {
         // Answer is incorrect
@@ -99,11 +99,41 @@ function checkAnswer(answer) {
     // Move on to the next question or end the quiz
 
     currentQuestionIndex++;
-    if (currentQuestionIndex === currentQuestionIndex.length) {
+    if (currentQuestionIndex === quizSettings.questions.length) {
       endQuiz();
     } else {
       displayQuestion();
     }
   }
 
+
   // End the quiz
+
+  function endQuiz() {
+    // Stop the timer
+    // clearInterval(timerInterval);
+    // Show the final score and hide the quiz content
+    var questionsScreen = document.getElementById("question-screen").style.visibility = "hidden";
+    document.getElementById("score").innerHTML = 'Your Score: ' + score
+    var finishScreen = document.getElementById("quiz-finish-screen").style.visibility = "visible"
+  }
+
+  // Submission
+// Define event listener for when the user submits their score
+submitBtn = document.getElementById("submit");
+const initialsInput = document.getElementById("initials");
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  // Get the user's initials and score
+  var initials = initialsInput.value.trim();
+  if (initials !== "") {
+    var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+    highScores.push({
+      initials: initials,
+      score: userScore,
+    });
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    // Redirect to high scores page
+    window.location.href = "highscores.html";
+  }
+});
